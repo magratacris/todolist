b8ffa7ec0d9a948600348581617042aa2d08f173
sain1aurent <crismagrata@gmail.com>
2023-04-03 11:10:25 +0800

    refactor: ui changes for mobile devices

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -29,7 +29,7 @@ const TaskList = () => {
         ${
           taskCompleted === 0
             ? "xs:max-h-[25rem]  sm:max-h-[29rem] md:max-h-[33rem]  2xl:max-h-[30rem] 3xl:max-h-[45rem]"
-            : "xs:max-h-[11rem]  sm:max-h-[18rem] 2xl:max-h-[14rem] 3xl:max-h-[23rem]"
+            : "xs:max-h-[13rem]  sm:max-h-[18rem] 2xl:max-h-[14rem] 3xl:max-h-[23rem]"
         } `}
       >
         {taskList?.map((list) => (
