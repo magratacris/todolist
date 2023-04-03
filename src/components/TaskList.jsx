a6e9e908e5d1899299d23864d8cdb0f77ea0f34f
sain1aurent <crismagrata@gmail.com>
2023-04-03 11:25:45 +0800

    refactor: final changes on ui

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -28,8 +28,8 @@ const TaskList = () => {
         className={`${listContainer} 
         ${
           taskCompleted === 0
-            ? "xs:max-h-[25rem]  sm:max-h-[29rem] md:max-h-[33rem]  2xl:max-h-[30rem] 3xl:max-h-[45rem]"
-            : "xs:max-h-[13rem]  sm:max-h-[18rem] 2xl:max-h-[14rem] 3xl:max-h-[23rem]"
+            ? "xs:max-h-[27rem]  sm:max-h-[34rem] md:max-h-[38rem] lg:max-h-[53rem] 2xl:max-h-[33rem] 3xl:max-h-[45rem]"
+            : "xs:max-h-[13rem]  sm:max-h-[18rem] 2xl:max-h-[16rem] 3xl:max-h-[23rem]"
         } `}
       >
         {taskList?.map((list) => (
